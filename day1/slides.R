f327400e4b02c1e19befe8b3f8d5656c5176305f
George G. Vega Yon <g.vegayon@gmail.com>
2020-08-12 09:52:07 -0700

    Updating slides day 1 and adding slides for day 2

--- day1/slides.R
+++ day1/slides.R
@@ -1,82 +1,100 @@
-## ----set-up------------------------------------------------------------------------------------------
+## ----set-up----------------------------------------------------------------
 library(ggplot2)
 data("diamonds")
 
 
-## ----head-of-diamonds-tibble, echo=FALSE-------------------------------------------------------------
+## ----head-of-diamonds-tibble, echo=FALSE-----------------------------------
 head(diamonds)
 
 
-## ----head-of-diamonds-dataframe, echo=FALSE----------------------------------------------------------
+## ----head-of-diamonds-dataframe, echo=FALSE--------------------------------
 head(as.data.frame(diamonds))
 
 
-## ----quick-data-look, results='hold'-----------------------------------------------------------------
+## ----quick-data-look, results='hold'---------------------------------------
 # How many rows and columns?
 nrow(diamonds)
 ncol(diamonds)
 
 
-## ----viz-0a, echo=FALSE, cache=TRUE------------------------------------------------------------------
+## ----viz-0a, echo=FALSE, cache=TRUE----------------------------------------
 ggplot(data = diamonds) +
-  geom_point(mapping = aes(x = carat, y = price, color = clarity)) +
-  facet_wrap(~color) + 
+  geom_point(mapping = aes(x = carat, y = price, color = color)) +
+  facet_wrap(~clarity) + 
   labs(
-    title    = "Price of Diamonds (by color)",
+    title    = "Price of Diamonds (by clarity)",
     subtitle = "data from the ggplot2 R package",
     x        = "Weight of the diamond (carat)",
     y        = "Price in US dollars",
-    color    = "Clarity from \n I1 (worst) to IF (best)"
+    color    = "Color from \n J (worst) to D (best)"
     )
 
 
-## ----viz-0b------------------------------------------------------------------------------------------
+## ----viz-0b----------------------------------------------------------------
 ggplot(data = diamonds)
 
 
-## ----viz-0c, eval = FALSE----------------------------------------------------------------------------
+## ----viz-0c, eval = FALSE--------------------------------------------------
 ## ggplot(data = diamonds) +
 ##   geom_point()
 
 
-## ----viz-1, cache=TRUE-------------------------------------------------------------------------------
+## ----viz-1, cache=TRUE-----------------------------------------------------
 ggplot(data = diamonds) +
   geom_point(mapping = aes(x = carat, y = price))
 
 
-## ----viz-2, cache=TRUE-------------------------------------------------------------------------------
+## ----viz-2, cache=TRUE-----------------------------------------------------
 ggplot(data = diamonds) +
-  geom_point(mapping = aes(x = carat, y = price, color = clarity))
+  geom_point(mapping = aes(x = carat, y = price, color = color))
 
 
-## ----viz-3, cache=TRUE-------------------------------------------------------------------------------
+## ----viz-3, cache=TRUE-----------------------------------------------------
 ggplot(data = diamonds) +
-  geom_point(mapping = aes(x = carat, y = price, color = clarity)) +
-  facet_wrap(~color)
+  geom_point(mapping = aes(x = carat, y = price, color = color)) +
+  facet_wrap(~clarity)
 
 
-## ----viz-4, cache=TRUE-------------------------------------------------------------------------------
+## ----viz-4, cache=TRUE-----------------------------------------------------
 ggplot(data = diamonds) +
-  geom_point(mapping = aes(x = carat, y = price, color = clarity)) +
-  facet_wrap(~color) + 
+  geom_point(mapping = aes(x = carat, y = price, color = color)) +
+  facet_wrap(~clarity) + 
   labs(
-    title    = "Price of Diamonds (by color)",
+    title    = "Price of Diamonds (by clarity)",
     subtitle = "data from the ggplot2 R package",
     x        = "Weight of the diamond (carat)",
     y        = "Price in US dollars",
-    color    = "Clarity from \n I1 (worst) to IF (best)"
+    color    = "Color from \n J (worst) to D (best)"
     )
 
 
-## ----mpg-sol, eval=FALSE, echo=FALSE-----------------------------------------------------------------
-## data(mpg)
-## ggplot(data = mpg) +
-##   geom_point(mapping = aes(x = displ, y = hwy, color = drv)) +
-##   labs(
-##     title    = "Fuel economy data",
-##     subtitle = "(1999 - 2008)",
-##     x        = "Engine displacement (liters)",
-##     y        = "Highway MPG",
-##     color    = "Drive train"
-##   )
+## ----viz-4-swapped, cache=TRUE---------------------------------------------
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = price, y = carat, color = color)) +
+  facet_wrap(~clarity) + 
+  labs(
+    title    = "Price of Diamonds (by clarity)",
+    subtitle = "data from the ggplot2 R package",
+    y        = "Weight of the diamond (carat)",
+    x        = "Price in US dollars",
+    color    = "Color from \n J (worst) to D (best)"
+    )
+
+
+## ----mpg-sol, eval=TRUE, echo=TRUE-----------------------------------------
+data(mpg)
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, color = drv)) +
+  labs(
+    title    = "Fuel economy data",
+    subtitle = "(1999 - 2008)",
+    x        = "Engine displacement (liters)",
+    y        = "Highway MPG",
+    color    = "Drive train"
+  )
+
+
+## ----example-w-boxplot, cache = TRUE---------------------------------------
+ggplot(data = diamonds) +
+  geom_boxplot(mapping = aes(x = clarity, y = price, fill = clarity)) 
 
--- day2/README.md
+++ day2/README.md
@@ -1,1 +1,2 @@
-placeholder
+Slides available <a href="https://rawcdn.githack.com/USCbiostats/rbootcamp/master/day1/intro2dplyr.html" target="_blank">here</a>
+
